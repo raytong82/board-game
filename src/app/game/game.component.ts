2dcf0731fad65b710e5ca232cc3ac2ac485332d5
raytong82 <raytong82@gmail.com>
2017-10-16 21:36:37 +0800

    server url for WS

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,7 +10,7 @@ import _ from "lodash";
 })
 export class GameComponent implements OnInit {
 
-  socket = io('https://ray-board-game.herokuapp.com');
+  socket = io(location.origin);
   user: any;
   joined: boolean = false;
   waiting: boolean = true;
